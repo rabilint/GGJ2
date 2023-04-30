56f69caec0763523a02e542aa25aff46e6ee3214
Nick <67823457+nickShaba@users.noreply.github.com>
2023-04-30 20:52:41 +0300

    база механiки гри
    при натисканнi на кнопку мишi дороги будуть рухатися

--- ggj2/Assets/code/spawnRoad.cs
+++ ggj2/Assets/code/spawnRoad.cs
@@ -6,21 +6,26 @@ using UnityEngine.UI;
 public class spawnRoad : MonoBehaviour
 {
     public GameObject[] roadsPrefab = new GameObject[4];
-    private GameObject[] arrayOfRoads = new GameObject[6];
+    [SerializeField]private GameObject[] arrayOfRoads = new GameObject[6];
 
     // public GameObject[] countdownNums = GameObject[4];
+    [Space]
     public Text countdownText;
     private int countdownTime = 3;
+    public GameObject emptyPrefab;
+
+    // private GameObject roads;
 
     void Start()
     {
-        InvokeRepeating("Countdown", 0.35f, 1.0f);
+        InvokeRepeating("Countdown", 0f, 1.0f);
+        SpawnRoads();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MainGameplay();
     }
 
     void Countdown()
@@ -36,5 +41,47 @@ public class spawnRoad : MonoBehaviour
         }
 
         countdownTime--;
+    }
+
+    void SpawnRoads()
+    {
+        float x = -4.32f;
+        float y = 0.5f;
+        
+        arrayOfRoads[0] = Instantiate(emptyPrefab, new Vector3(-7.32f,y,0), Quaternion.identity);
+        arrayOfRoads[0].name = "null_" + 0;
+        for(int i = 1; i < arrayOfRoads.Length; i++)
+        {
+            int num = Random.Range(0,4);
+
+            arrayOfRoads[i] = Instantiate(roadsPrefab[num], new Vector3(x,y,0), Quaternion.identity);
+            arrayOfRoads[i].name = "road_" + i;
+            x += 3;
+        }
+    }
+
+    void MainGameplay()
+    {
+        if(countdownTime <= 0 && Input.GetMouseButtonDown(0)) 
+            MoveRoads();
+    }
+
+    void MoveRoads()
+    {
+        for(int i = 0; i < arrayOfRoads.Length; i++)
+        {
+            if(arrayOfRoads[i].transform.position.x <= -7.32f)
+            {
+                Destroy(arrayOfRoads[i]);
+
+                int num = Random.Range(0,4);
+                arrayOfRoads[i] = Instantiate(roadsPrefab[num], new Vector2(7.68f, 0.5f), Quaternion.identity);
+                // Debug.Log(i);
+            } else {
+                Vector3 newPosition = arrayOfRoads[i].transform.position;
+                newPosition.x -= 3f;
+                arrayOfRoads[i].transform.position = newPosition;
+            }
+        }
     }
 }
